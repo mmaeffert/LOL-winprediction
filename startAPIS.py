aede9f467e7a6bd64cdad4cd03ec6231bb862e71
unknown <m.maeffert @outlook.de>
2022-07-20 13:54:03 +0200

    fix

--- startAPIS.py
+++ startAPIS.py
@@ -3,5 +3,5 @@ import os
 
 print(len(sys.argv))
 
-for i in range(3, len(sys.argv) + 1):
+for i in range(1, len(sys.argv)):
     os.system("nohup python3 main.py " + sys.argv[i] + " " + str(i - 2) + " " + str(i) + " > nohup" + str(i) + ".log &")
